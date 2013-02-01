14f0d152d5bb42d73de34ca03b9c9c6c2228fa01
Jose Manuel Cristobal Cristobal <jmcristobal@incita.eu>
2013-02-01 14:38:22 +0100

    [SRC] DeviceStateReceiver refactor

--- SipPhone/src/com/jmcristobal/sipphone/DeviceStateReceiver.java
+++ SipPhone/src/com/jmcristobal/sipphone/DeviceStateReceiver.java
@@ -22,90 +22,31 @@ package com.jmcristobal.sipphone;
  */
 
 import android.content.BroadcastReceiver;
-import android.content.ContentUris;
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 
-import com.csipsimple.api.SipManager;
-import com.csipsimple.api.SipProfile;
 import com.csipsimple.service.SipService;
-import com.csipsimple.utils.CallHandlerPlugin;
-import com.csipsimple.utils.ExtraPlugins;
 import com.csipsimple.utils.Log;
-import com.csipsimple.utils.NightlyUpdater;
-import com.csipsimple.utils.PhoneCapabilityTester;
 import com.csipsimple.utils.PreferencesProviderWrapper;
-import com.csipsimple.utils.RewriterPlugin;
 
 public class DeviceStateReceiver extends BroadcastReceiver {
 
-	private static final String THIS_FILE = "DeviceStateReceiver";
-	public static final String APPLY_NIGHTLY_UPLOAD = "com.csipsimple.action.APPLY_NIGHTLY";
+	private static final String TAG = "DeviceStateReceiver";
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		PreferencesProviderWrapper prefWrapper = new PreferencesProviderWrapper(context);
 		String intentAction = intent.getAction();
-		Log.d(THIS_FILE, intentAction);
-		//
-		// ACTION_DATA_STATE_CHANGED
-		// Data state change is used to detect changes in the mobile
-		// network such as a switch of network type (GPRS, EDGE, 3G)
-		// which are not detected by the Connectivity changed broadcast.
-		//
-		//
-		// ACTION_CONNECTIVITY_CHANGED
-		// Connectivity change is used to detect changes in the overall
-		// data network status as well as a switch between wifi and mobile
-		// networks.
-		//
-		if (/* intentAction.equals(ACTION_DATA_STATE_CHANGED) || */
-		intentAction.equals(ConnectivityManager.CONNECTIVITY_ACTION)
+		Log.d(TAG, intentAction);
+		if (intentAction.equals(ConnectivityManager.CONNECTIVITY_ACTION)
 				|| intentAction.equals(Intent.ACTION_BOOT_COMPLETED)) {
-
 			if (prefWrapper.isValidConnectionForIncoming()
 					&& !prefWrapper.getPreferenceBooleanValue(PreferencesProviderWrapper.HAS_BEEN_QUIT)) {
-				Log.d(THIS_FILE, "Try to start service if not already started");
+				Log.d(TAG, "Try to start service if not already started");
 				Intent sip_service_intent = new Intent(context, SipService.class);
 				context.startService(sip_service_intent);
 			}
-
-		} else if (intentAction.equals(SipManager.INTENT_SIP_ACCOUNT_ACTIVATE)) {
-			context.enforceCallingOrSelfPermission(SipManager.PERMISSION_CONFIGURE_SIP, null);
-
-			long accId;
-			accId = intent.getLongExtra(SipProfile.FIELD_ID, SipProfile.INVALID_ID);
-
-			if (accId == SipProfile.INVALID_ID) {
-				// allow remote side to send us integers.
-				// previous call will warn, but that's fine, no worries
-				accId = intent.getIntExtra(SipProfile.FIELD_ID, (int) SipProfile.INVALID_ID);
-			}
-
-			if (accId != SipProfile.INVALID_ID) {
-				boolean active = intent.getBooleanExtra(SipProfile.FIELD_ACTIVE, true);
-				ContentValues cv = new ContentValues();
-				cv.put(SipProfile.FIELD_ACTIVE, active);
-				int done = context.getContentResolver().update(
-						ContentUris.withAppendedId(SipProfile.ACCOUNT_ID_URI_BASE, accId), cv, null, null);
-				if (done > 0) {
-					if (prefWrapper.isValidConnectionForIncoming()) {
-						Intent sipServiceIntent = new Intent(context, SipService.class);
-						context.startService(sipServiceIntent);
-					}
-				}
-			}
-		} else if (Intent.ACTION_PACKAGE_ADDED.equalsIgnoreCase(intentAction)
-				|| Intent.ACTION_PACKAGE_REMOVED.equalsIgnoreCase(intentAction)) {
-			CallHandlerPlugin.clearAvailableCallHandlers();
-			RewriterPlugin.clearAvailableRewriters();
-			ExtraPlugins.clearDynPlugins();
-			PhoneCapabilityTester.deinit();
-		} else if (APPLY_NIGHTLY_UPLOAD.equals(intentAction)) {
-			NightlyUpdater nu = new NightlyUpdater(context);
-			nu.applyUpdate(intent);
 		}
 	}
 
